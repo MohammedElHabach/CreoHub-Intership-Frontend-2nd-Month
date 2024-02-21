8e78ee4f9f2a086da3579ca09e4a0e83814f1797
Mohammed El Habach <mhamadhabach06@gmail.com>
2024-02-21 12:58:37 +0200

    fetch data

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,7 +3,7 @@ import React from "react";
 const Card = ({img,title,desc}) => {
   return (
     <div className="space-y-3 card ">
-      <img className="" src={img} alt="image" />
+      <img className="imgCard" src={img} alt="image" />
       <h2 className="text-2xl">{title}</h2>
       <p>{desc}</p>
     </div>
--- src/components/DataSection.js
+++ src/components/DataSection.js
@@ -1,35 +1,17 @@
 import React from 'react'
 import Card from "@/components/Card"
+import Link from 'next/link'
 
 const DataSection = ({data}) => {
-    // const Data = [
-    //     {
-    //         img:"/pepsi.jpg",
-    //         title:"Fizz the World",
-    //         description:"Pepsi's Expo 2024"
-    //     },
-    //     {
-    //         img:"/pepsi.jpg",
-    //         title:"Fizz the World",
-    //         description:"Pepsi's Expo 2024.Lorem ipsum dolor sit amet consectetur adipisicing elit. "
-    //      },
-    //      {
-    //         img:"/pepsi.jpg",
-    //         title:"Fizz the World",
-    //         description:"Pepsi's Expo 2024.Lorem ipsum dolor sit amet consectetur adipisicing elit.Quaerat tenetur commodi sed, sunt omnis "
-    //      },
-    //      {
-    //         img:"/pepsi.jpg",
-    //         title:"Fizz the World",
-    //         description:"Pepsi's Expo 2024.Lorem ipsum dolor sit amet consectetur adipisicing elit.Quaerat tenetur commodi sed, sunt omnis,adipisicing elit.Quaerat tenetur commodi sed, sunt omnis "
-    //      },
-    // ]
+
   return (
     <section id='data section'>
         <div className='xl:container mx-auto mt-20 px-12'>
             <div className='grid grid-cols-1 sm:grid-cols-2 place-items-center gap-7 '>
                 {data.map((elt) => (
-                    <Card key={elt.id} img={elt.details.image} title={elt.title} desc={elt.subtitle}/>
+                    <Link key={elt.id} href={elt.details.cta_link} target='_blank'>
+                        <Card  img={elt.details.image} title={elt.title} desc={elt.details.cta_text}/>
+                    </Link>
                 ))}
             </div>
         </div>
