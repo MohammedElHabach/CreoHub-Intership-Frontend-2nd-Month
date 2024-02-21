4dc6270176ae6d8e21746cd51149a3b3f1389e34
Mohammed El Habach <mhamadhabach06@gmail.com>
2024-02-21 13:11:44 +0200

    fetch data

--- src/components/DataSection.js
+++ src/components/DataSection.js
@@ -7,7 +7,7 @@ const DataSection = ({data}) => {
   return (
     <section id='data section'>
         <div className='xl:container mx-auto mt-20 px-12'>
-            <div className='grid grid-cols-1 sm:grid-cols-2 place-items-center gap-7 '>
+            <div className='grid grid-cols-1 sm:grid-cols-2  gap-7 '>
                 {data.map((elt) => (
                     <Link key={elt.id} href={elt.details.cta_link} target='_blank'>
                         <Card  img={elt.details.image} title={elt.title} desc={elt.details.cta_text}/>
