a25c6a6cd773be2ae9f06ec56aefec02f2bd0686
Mohammed El Habach <mhamadhabach06@gmail.com>
2024-02-21 12:18:11 +0200

    fetch data

---
+++ src/app/page.js
@@ -0,0 +1,39 @@
+// "use client"
+import Navbar from "@/components/Navbar";
+import HeroSection from "@/components/HeroSection";
+import DataSection from "@/components/DataSection";
+import Footer from "@/components/Footer";
+import axios from "@/utils/axios";
+
+export default async function Home() {
+  
+  const data = await getData()
+  // console.log(data.props.data.data.sections);
+  
+    return (
+    <>
+      <Navbar />
+      <HeroSection/>
+      <DataSection data={data.props.data.data.sections}/>
+      <Footer/>
+    </>
+  );
+}
+
+async function getData() {
+  let data;
+  await axios
+    .get("/page/news", {
+      headers: {
+        "Accept-Language": "en", 
+      },
+    })
+    .then((res) => {
+      data = res.data;
+    })
+    .catch(console.error);
+
+
+  return { props: { data } };
+}
+
--- src/components/DataSection.js
+++ src/components/DataSection.js
@@ -1,34 +1,35 @@
 import React from 'react'
 import Card from "@/components/Card"
-const DataSection = () => {
-    const Data = [
-        {
-            img:"/pepsi.jpg",
-            title:"Fizz the World",
-            description:"Pepsi's Expo 2024"
-        },
-        {
-            img:"/pepsi.jpg",
-            title:"Fizz the World",
-            description:"Pepsi's Expo 2024.Lorem ipsum dolor sit amet consectetur adipisicing elit. "
-         },
-         {
-            img:"/pepsi.jpg",
-            title:"Fizz the World",
-            description:"Pepsi's Expo 2024.Lorem ipsum dolor sit amet consectetur adipisicing elit.Quaerat tenetur commodi sed, sunt omnis "
-         },
-         {
-            img:"/pepsi.jpg",
-            title:"Fizz the World",
-            description:"Pepsi's Expo 2024.Lorem ipsum dolor sit amet consectetur adipisicing elit.Quaerat tenetur commodi sed, sunt omnis,adipisicing elit.Quaerat tenetur commodi sed, sunt omnis "
-         },
-    ]
+
+const DataSection = ({data}) => {
+    // const Data = [
+    //     {
+    //         img:"/pepsi.jpg",
+    //         title:"Fizz the World",
+    //         description:"Pepsi's Expo 2024"
+    //     },
+    //     {
+    //         img:"/pepsi.jpg",
+    //         title:"Fizz the World",
+    //         description:"Pepsi's Expo 2024.Lorem ipsum dolor sit amet consectetur adipisicing elit. "
+    //      },
+    //      {
+    //         img:"/pepsi.jpg",
+    //         title:"Fizz the World",
+    //         description:"Pepsi's Expo 2024.Lorem ipsum dolor sit amet consectetur adipisicing elit.Quaerat tenetur commodi sed, sunt omnis "
+    //      },
+    //      {
+    //         img:"/pepsi.jpg",
+    //         title:"Fizz the World",
+    //         description:"Pepsi's Expo 2024.Lorem ipsum dolor sit amet consectetur adipisicing elit.Quaerat tenetur commodi sed, sunt omnis,adipisicing elit.Quaerat tenetur commodi sed, sunt omnis "
+    //      },
+    // ]
   return (
     <section id='data section'>
         <div className='xl:container mx-auto mt-20 px-12'>
             <div className='grid grid-cols-1 sm:grid-cols-2 place-items-center gap-7 '>
-                {Data.map((elt,index) => (
-                    <Card key={index} img={elt.img} title={elt.title} desc={elt.description}/>
+                {data.map((elt) => (
+                    <Card key={elt.id} img={elt.details.image} title={elt.title} desc={elt.subtitle}/>
                 ))}
             </div>
         </div>
@@ -36,4 +37,7 @@ const DataSection = () => {
   )
 }
 
+
+
 export default DataSection
+
