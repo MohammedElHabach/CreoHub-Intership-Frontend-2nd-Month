1e47b741a258e3908b2b2f7714573d25f5305bb8
Mohammed El Habach <mhamadhabach06@gmail.com>
2024-02-28 11:38:50 +0200

    about us

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,25 +1,23 @@
-import Navbar from "../../components/Navbar"
-import AboutSection from "@/components/AboutSection"
+import Navbar from "../../components/Navbar";
+import AboutSection from "@/components/AboutSection";
 const about = () => {
-
-    const aboutText1 = "We Foster \n Innovation , Safeguarding Your Legacy";
-    const img = "/office.avif"
-
-    const aboutText2 = "We Foster \n Innovation , Unleash Your Legacy";
-    const img2 = "/office2.avif"
-
-    const pos1 = "-right-28"; 
-    const pos2 = "-right-24"; 
-
-
   return (
     <>
-    <Navbar/>
-    <AboutSection rightPos={pos1} img={img} text={aboutText1} span={`\nFor Future Generations`}/>
-    <AboutSection rightPos={pos2} img={img2} text={aboutText2} span={`\nFor Future Generations`}/>
-
+      <Navbar />
+      <AboutSection
+        rightPos={"right-32"}
+        img={"/office.avif"}
+        text={"We Foster \n Innovation , Safeguarding Your Legacy"}
+        span={`\nFor Future Generations`}
+      />
+      <AboutSection
+        leftPos={"left-40"}
+        img={"/office2.avif"}
+        text={"We Foster \n Innovation , Unleash Your Legacy"}
+        span={`\nFor Future Generations`}
+      />
     </>
-  )
-}
+  );
+};
 
-export default about
+export default about;
--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,14 +1,14 @@
 import React from 'react'
 
-const AboutSection = ({text,span,img,rightPos}) => {
+const AboutSection = ({text,span,img,rightPos,leftPos}) => {
 
 
   return (
     <section id='about'>
         <div className='xl:container mx-auto  px-12 mt-10 '>
-            <div className='flex   items-center relative'>
-                <img className='w-3/4' src={img} alt="image" />
-                <h1 className={`text-3xl whitespace-pre-line absolute ${rightPos}  `}>{text}<span className='text-red-500'>{span}</span></h1>
+            <div className={`flex ${rightPos ? '':'flex-row-reverse'}   items-center relative`}>
+                <img className='w-[800px]' src={img} alt="image" />
+                <h1 className={`text-xl sm:text-3xl whitespace-pre-line absolute ${rightPos} ${leftPos}  `}>{text}<span className='text-red-500'>{span}</span></h1>
             </div>
         </div>
 
