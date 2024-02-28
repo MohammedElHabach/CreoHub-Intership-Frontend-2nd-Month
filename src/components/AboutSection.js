bbe31a4bba1ff51c6ce534017477635ca4551f69
Mohammed El Habach <mhamadhabach06@gmail.com>
2024-02-28 13:50:41 +0200

    about us

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -8,7 +8,7 @@ const AboutSection = ({text,span,img,rightPos,leftPos}) => {
         <div className='xl:container mx-auto  px-12 mt-10 '>
             <div className={`flex flex-col-reverse   ${rightPos ? 'md:flex-row':'md:flex-row-reverse'}   items-center relative`}>
                 <img className='w-[920px] h-[500px] pt-4 md:pt-0' src={img} alt="image" />
-                <h1 className={`text-2xl whitespace-pre-line md:absolute  ${rightPos} ${leftPos}  `}>{text}<span className='text-red-500'>{span}</span></h1>
+                <h1 className={`text-xl md:text-2xl text-center  md:text-start whitespace-pre-line md:absolute  ${rightPos} ${leftPos}  `}>{text}<span className='text-red-500'>{span}</span></h1>
             </div>
         </div>
 
