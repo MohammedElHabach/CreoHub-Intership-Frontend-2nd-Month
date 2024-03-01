89d342da5100962499c62a04422ce57ce3478c55
Mohammed El Habach <mhamadhabach06@gmail.com>
2024-03-01 14:39:43 +0200

    contact

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -8,7 +8,7 @@ const page = () => {
     <section id='contact'>
         <div className='xl:container mx-auto px-12 mt-10'>
             <div className='text-center '>
-                <h1 className='text-3xl font-bold  decoration-pink underline-offset-[20px]	 underline'>Contact Us</h1> 
+                <h1 className='text-3xl font-bold relative after:content-[""] after:absolute after:bg-pink after:h-0.5 after:-bottom-3 after:w-20 after:left-[38%] sm:after:left-[44%] md:after:left-[45%] lg:after:left-[46%] xl:after:left-[47.5%]'>Contact Us</h1> 
                 <form className='space-y-10  mt-10'>
                     <input placeholder='Name' className='p-3 mx-auto block bg-slate-800 rounded-md outline-none' type="text" />
                     <input placeholder='Email' type="email" className='p-3  mx-auto block bg-slate-800 rounded-md outline-none' />
