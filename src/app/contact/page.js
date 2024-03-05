0c51ac25fb3d3e4734dba2531e8e4454daaed09c
Mohammed El Habach <mhamadhabach06@gmail.com>
2024-03-05 10:58:51 +0200

    from validation

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -64,7 +64,7 @@ const page = () => {
             <h1 className='text-3xl font-bold relative after:content-[""] after:absolute after:bg-pink after:h-0.5 after:-bottom-3 after:w-20 after:left-[38%] sm:after:left-[44%] md:after:left-[45%] lg:after:left-[46%] xl:after:left-[47.5%]'>
               Contact Us
             </h1>
-            <form onSubmit={handleSubmit} className="space-y-10  mt-10">
+            <form onSubmit={handleSubmit} className={`${errors.name || errors.email || errors.message ? 'space-y-5':'space-y-10 '} mt-10`}>
               <input
                 name="name"
                 placeholder="Name"
