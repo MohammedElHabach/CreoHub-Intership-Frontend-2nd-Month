3b1deea876ed251eb726e2f8e3bb3362f4e26ea1
Mohammed El Habach <mhamadhabach06@gmail.com>
2024-03-06 11:28:47 +0200

    animation

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,15 +1,14 @@
 "use client"
 import gsap, {  Sine } from "gsap";
-import React, { useEffect, useRef } from "react";
+import React, { useEffect } from "react";
 
 const Card = ({img,title,desc}) => {
-  let card = useRef(null)
 
   useEffect(()=>{
-    gsap.to(card,2.5,{opacity:1,y:-40,ease:Sine.out})
+    gsap.to("#card",2.5,{opacity:1,y:-40,ease:Sine.out})
   },[])
   return (
-    <div ref={el => {card=el}}  className="space-y-3  opacity-0">
+    <div  id="card" className="space-y-3  opacity-0">
       <img className="sm:h-[500px] object-cover" src={img} alt="image" />
       <h2 className="text-2xl">{title}</h2>
       <p>{desc}</p>
