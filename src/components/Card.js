83481ac9d69ce6a3634e3fcda498867474c27eef
Mohammed El Habach <mhamadhabach06@gmail.com>
2024-03-06 11:18:04 +0200

    animation

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,8 +1,15 @@
-import React from "react";
+"use client"
+import gsap, {  Sine } from "gsap";
+import React, { useEffect, useRef } from "react";
 
 const Card = ({img,title,desc}) => {
+  let card = useRef(null)
+
+  useEffect(()=>{
+    gsap.to(card,2.5,{opacity:1,y:-40,ease:Sine.out})
+  },[])
   return (
-    <div className="space-y-3 ">
+    <div ref={el => {card=el}}  className="space-y-3  opacity-0">
       <img className="sm:h-[500px] object-cover" src={img} alt="image" />
       <h2 className="text-2xl">{title}</h2>
       <p>{desc}</p>
--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,8 +1,15 @@
-import React from 'react'
+"use client"
+import { TweenMax,Power3 } from 'gsap'
+import React, { useEffect, useRef } from 'react'
 
 const HeroSection = () => {
+  let banner = useRef(null)
+
+  useEffect(()=>{
+    TweenMax.to(banner,.9,{opacity:1,y:-30,ease:Power3.easeOut})
+  },[])
   return (
-    <section id='hero' >
+    <section ref={el => {banner = el}} id='hero' className=' opacity-0' >
         <div className='xl:container mx-auto mt-16 px-12 flex justify-center items-center relative'>
             <img className='' src="/bannerbg.png" alt="banner" />
             <h1 className='absolute leading-relaxed sm:leading-relaxed text-3xl sm:text-4xl md:text-5xl md:leading-relaxed   '>Here’s a teeny-weeny <br /> glimpse of our work. <br /> What’s coming later is <br /> <span className='text-pink font-bold ml-16 md:ml-28'>even </span> greater</h1>
